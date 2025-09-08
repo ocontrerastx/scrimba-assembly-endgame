09cb63f1961c89cfefa00927bb7c6ef9b630376f
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-09-08 15:19:38 -0500

    Added all content sections and styled them to match design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
+import { languages } from "../languages";
 import "./App.css";
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const languageElements = languages.map((language) => {
+    const styles = {
+      backgroundColor: language.backgroundColor,
+      color: language.color,
+    };
+
+    return (
+      <span key={language.name} style={styles} className="chip">
+        {language.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord.split("").map((letter, index) => (
+    <span key={index} className="letter">
+      {letter}
+    </span>
+  ));
+
+  const keyboardElements = alphabet
+    .split("")
+    .map((letter, index) => <button key={index}>{letter}</button>);
+
   return (
-    <>
+    <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>
@@ -11,7 +39,15 @@ function App() {
           from Assembly!
         </p>
       </header>
-    </>
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
+    </main>
   );
 }
 
